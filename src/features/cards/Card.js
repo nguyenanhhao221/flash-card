cb1b74957f3f3c83ee7d2108fc950f571e1ac720
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-05-05 21:43:08 +0700

    fix unused variable

--- src/features/cards/Card.js
+++ src/features/cards/Card.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
 import { selectCards } from "./cardSlice";
 
 export default function Card({ id }) {
